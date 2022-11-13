73f2b7d94957d37d0edeab997826a7306a460844
JamesAGV <102199710+JamesAGV@users.noreply.github.com>
2022-11-13 13:17:46 -0500

    Update preguntas.py
    Corrección pregunta 1

--- preguntas.py
+++ preguntas.py
@@ -64,27 +64,27 @@ def pregunta_01():
     En esta función se realiza la carga de datos.
     """
     # Lea el archivo `mushrooms.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('mushrooms.csv')
+     df = pd.read_csv('mushrooms.csv')
 
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
     #____.____(____)
-    df.drop(labels='veil-type', axis=1, inplace=True)
+     df.drop(labels='veil-type', axis=1, inplace=True)
 
     # Asigne la columna `type` a la variable `y`.
     #____ = ____
-    y = df.type
+     y = df.type
 
     # Asigne una copia del dataframe `df` a la variable `X`.
     #____ = ____.____(____)
-    X = df.copy()
+     X = df.copy()
 
     # Remueva la columna `type` del DataFrame `X`.
     #____.____(____)
-    X.drop(labels='type', axis=1, inplace=True)
+     X.drop(labels='type', axis=1, inplace=True)
 
     # Retorne `X` y `y`
-    return X, y
+     return X, y
 
 
 def pregunta_02():
